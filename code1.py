95bd1e1d26ea6f7a5cc4b6dff76df98915d2a383
ohsumin2468264 <104684783+ohsumin2468264@users.noreply.github.com>
2022-05-23 12:31:22 +0900

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,14 @@
+print("5개의 정수를 압력하세요.")
+integer1=(input("정수 1의 값을 입력하시오. : ")
+integer2=(input("정수 2의 값을 입력하시오. : ")
+integer3=(input("정수 3의 값을 입력하시오. : ")
+integer4=(input("정수 4의 값을 입력하시오. : ")
+integer5=(input("정수 5의 값을 입력하시오. : ")
+
+sum=integer1+integer2+integer3+integer4+integer5
+
+
+
+
+
+
---
+++ code3.py
@@ -0,0 +1,4 @@
+pay_rate = 9160
+h_word = int(input("일한 시간 입력 : ")
+mon_pay = h_work * pay_rate
+print(mon_pay)
